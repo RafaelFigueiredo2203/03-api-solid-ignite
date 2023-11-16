eecaa218187606661c7f60e359f83b3410f877da
RafaelFigueiredo2203 <rafaelfigueiedojunior7@gmail.com>
2023-11-16 18:07:58 -0300

    first commit

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchemma = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchemma.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables 😒', _env.error.format())
+
+  throw new Error('Invalid environment variables 😒')
+}
+export const env = _env.data
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { env } from '.././src/env'
+import { app } from './app'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Servidor rodando 🚀')
+  })
