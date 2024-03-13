97297a66def38d94db7cb3a43413ccb6d49ea9a3
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-03-13 16:27:00 -0300

    final

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,11 +1,30 @@
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+//postgresql://docker:docker@localhost:5432/ignitenode03?schema=public
+
+function generateDatabaseURL(schema:string){
+  if (!process.env.DATABASE_URL){
+    throw new Error('Please provide a DATABASE_URL environment variable ')
+    
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name:'prisma',
   transformMode: 'ssr',
   async setup() {
-      console.log('Executou')
+      const schema = randomUUID()
+      const databaseURL = generateDatabaseURL(schema)
 
+      execSync('npx prisma migrate deploy')
       return{
       async  teardown() {
           console.log('teradown')
