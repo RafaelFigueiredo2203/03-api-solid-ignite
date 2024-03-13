eccec654c3d061e675cb9f46dff7207994aef607
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-03-13 10:29:41 -0300

    Criando test Environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    dir: 'src', // Essa linha
+  },
 })
